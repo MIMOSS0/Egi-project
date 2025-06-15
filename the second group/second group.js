235a1b3c15f3b92805c2df82eac785c416316dd1
MIMOSS0 <mimo1852003@gmail.com>
2025-06-15 17:09:52 +0300

    Add files via upload

---
+++ the second group/second group.js
@@ -0,0 +1,51 @@
+window.onload = function() { 
+    var urlParams = new URLSearchParams(window.location.search); 
+    var name = urlParams.get('name');
+    var studentId = urlParams.get('studentId');
+
+    if (name) { 
+        document.getElementById("name1").textContent = name; 
+    }
+
+    if (studentId) {
+        document.getElementById('studentIdDisplay').textContent = 'الرقم الجامعي: ' + studentId;
+    } else {
+        document.getElementById('studentIdDisplay').textContent = 'لم يتم تسجيل الدخول.';
+    }
+}
+
+document.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', function(event) {
+        event.stopPropagation();
+    });
+});
+
+
+
+function uploadFile() {
+    const fileInput1 = document.getElementById('fileInput1');
+    const file = fileInput1.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(event) {
+            const studentName = document.getElementById('name1').textContent;
+            const studentId = document.getElementById('studentIdDisplay').textContent;
+            const fileData = event.target.result;
+            
+           
+            const fileList = JSON.parse(localStorage.getItem('fileList2')) || [];
+            fileList.push({ studentName, studentId, fileData });
+            localStorage.setItem('fileList2', JSON.stringify(fileList));
+            
+           
+            document.getElementById('message').textContent = 'تم إرسال الملف بنجاح';
+            
+           
+            fileInput1.value = '';
+        };
+        reader.readAsDataURL(file);
+    } else {
+        alert('يرجى اختيار ملف أولاً.');
+    }
+}
+
